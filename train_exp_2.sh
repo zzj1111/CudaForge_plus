7b7de009a3e792d92c2a95811f0eb2804596af46
Zijian Zhang <86014379+zzj1111@users.noreply.github.com>
2025-12-21 00:15:37 -0600

    Update train_exp_2.sh

--- train_exp_2.sh
+++ train_exp_2.sh
@@ -1,13 +1,30 @@
-export CUDA_VISIBLE_DEVICES=6,7
-export REWARD_CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export REWARD_CUDA_VISIBLE_DEVICES=5
 export WANDB_API_KEY="b8f38344ec7231ee89baa74ef7209dd5a43df6b2"
 export WANDB_ENTITY="mhong-university-of-minnesota"
 #export Model_path="/code/hongpaul-sandbox/temp/CudaForge_plus/verl/data/Qwen3_8b"
-export Model_path="/home/zha00175/data/Qwen3_8b"
+#export Model_path="/home/zha00175/data/zha00175/Qwen3-30B-A3B"
+export Model_path="Qwen/Qwen3-8B"
+
+export RAY_DEDUP_LOGS=0
+export HYDRA_FULL_ERROR=1
+export RAY_BACKEND_LOG_LEVEL=debug  # 有些版本支持
+
+max_response_length=16384
+
+loss_mode=gspo
+loss_agg_mode="seq-mean-token-mean"
+
+project_name=CudaForge_RL
+exp_name="d1220r2_DAPO_kernelbenchlevel1_topp080"
+
+
+CKPTS_DIR=/code/hongpaul-sandbox/temp/CudaForge_plus/${project_name}/${exp_name}
+
+source /mnt/data1/zha00175/miniconda/bin/activate
+conda activate verl
 
 mkdir -p logs
-LOGFILE="logs/qwen3_8k.txt"
-exec > "$LOGFILE" 2>&1
 
 
 # export VLLM_ATTENTION_BACKEND=XFORMERS
@@ -19,7 +36,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
  data.val_files=./dataset/CudaForge/Level1/test.parquet \
  data.train_batch_size=16 \
  data.max_prompt_length=8192 \
- data.max_response_length=8192 \
+ data.max_response_length=16384 \
  actor_rollout_ref.model.path=$Model_path \
  actor_rollout_ref.actor.optim.lr=1e-6 \
  actor_rollout_ref.actor.ppo_mini_batch_size=16 \
@@ -31,9 +48,9 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
  actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
  actor_rollout_ref.actor.fsdp_config.param_offload=False \
  actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
- actor_rollout_ref.rollout.temperature=0.6 \
+ actor_rollout_ref.rollout.temperature=0.7 \
  actor_rollout_ref.rollout.top_k=20 \
- actor_rollout_ref.rollout.top_p=0.95 \
+ actor_rollout_ref.rollout.top_p=0.8 \
  actor_rollout_ref.actor.clip_ratio_low=0.2 \
  actor_rollout_ref.actor.clip_ratio_high=0.28 \
  critic.optim.lr=1e-5 \
@@ -46,10 +63,10 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
  trainer.val_before_train=False \
  trainer.n_gpus_per_node=2 \
  trainer.nnodes=1 \
+ trainer.default_local_dir="${CKPTS_DIR}" \
  trainer.save_freq=20 \
  trainer.test_freq=100 \
  trainer.logger='["console","wandb"]' \
  trainer.project_name="DAPO" \
- trainer.experiment_name="d1214r2_DAPO_kernelbenchlevel1_topp095" \
+ trainer.experiment_name="d1220r2_DAPO_kernelbenchlevel1_topp080" \
  trainer.total_epochs=20 \
-#  2>&1 | tee ./outputs/log/8k.log
